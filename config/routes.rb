902bdeb5bebd0a309750e75ae14eab9d565f6b3c
Matt Gaudio <mgaudio15@gmail.com>
2016-09-19 15:15:38 -0400

    huge  commit not sure what is in it

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,36 @@
 Rails.application.routes.draw do
-  resources :orders
-  resources :line_items
-  resources :carts
+
+  get 'admin' => 'admin#index'
+
+  controller :sessions do 
+    get 'login' => :new 
+    post 'login' => :create 
+    delete 'logout' => :destroy
+  end
+
+  get "sessions/create"
+  get "sessions/destroy"
+
+  resources :users
 
   get 'store/index' 
   resources :products do 
     get :who_bought, on: :member 
   end
   
-
-  resources :products
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   # Custom
-  root "store#index"
+
+  scope '(:locale)' do 
+    resources :orders
+    resources :line_items
+    resources :carts
+    root "store#index"
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
